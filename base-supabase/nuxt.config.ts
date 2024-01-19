b1f0b98bc26d91f659b90262b5fe935a56d15aa5
Eunjae Lee <hey@eunjae.dev>
2024-01-19 21:47:31 +0100

    change repo name

--- base-supabase/nuxt.config.ts
+++ base-supabase/nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ['../base'],
   // @ts-expect-error
   supabase: {},
 })
